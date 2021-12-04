6323d22273ece0229726ffa690b401597c0bbba8
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-12-03 20:21:39 -0600

    Create homepage setup

--- controllers/html/home-routes.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const router = require('express').Router();
-
-router.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname, '../../public/index.html'));
-});
